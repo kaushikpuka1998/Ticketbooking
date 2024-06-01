0be9302573a22622c38897b608bc14404614b72b
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2024-06-01 08:05:39 +0530

    creating a allseats mapping with a show and all shows and shows finding by id api added

--- src/main/java/com/kgstrivers/TicketMasterBooking/Controllers/ShowController.java
+++ src/main/java/com/kgstrivers/TicketMasterBooking/Controllers/ShowController.java
@@ -4,6 +4,8 @@ import com.kgstrivers.TicketMasterBooking.models.Show;
 import com.kgstrivers.TicketMasterBooking.services.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,20 @@ public class ShowController {
     @PostMapping("/addShow")
     public ResponseEntity addShow(@RequestBody Show show) {
         return ResponseEntity.ok(showService.addShow(show));
+    }
+
+    @GetMapping("/getShow/{showId}")
+    public ResponseEntity getShow(@PathVariable Long showId) {
+        return ResponseEntity.ok(showService.getShow(showId));
+    }
+
+    @GetMapping("/getShows/")
+    public ResponseEntity getShows() {
+        return ResponseEntity.ok(showService.getShows());
+    }
+
+    @GetMapping("/allSeats/{showId}")
+    public ResponseEntity allSeats(@PathVariable Long showId) {
+        return ResponseEntity.ok(showService.allSeats(showId));
     }
 }
--- src/main/java/com/kgstrivers/TicketMasterBooking/services/ShowService.java
+++ src/main/java/com/kgstrivers/TicketMasterBooking/services/ShowService.java
@@ -1,5 +1,6 @@
 package com.kgstrivers.TicketMasterBooking.services;
 
+import com.kgstrivers.TicketMasterBooking.models.Seat;
 import com.kgstrivers.TicketMasterBooking.models.Show;
 import com.kgstrivers.TicketMasterBooking.reposirories.ShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,18 @@ public class ShowService {
 
     public Show addShow(Show show) {
         return showRepository.save(show);
+    }
+
+    public Show getShow(Long showId) {
+        return showRepository.findById(showId).orElse(null);
+    }
+
+    public Iterable<Show> getShows() {
+        return showRepository.findAll();
+    }
+
+    public Iterable<Seat> allSeats(Long showId) {
+        return showRepository.findById(showId).orElse(null).getSeats();
     }
 
 }
